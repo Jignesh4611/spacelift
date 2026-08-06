4729cf04d4e5e17046306df00a4348c8dfc7fb08
Jignesh4611 <solankijignesh4611@gmail.com>
2026-08-06 09:05:54 +0000

    change

---
+++ terraform/main.tf
@@ -0,0 +1,48 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  owners = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_security_group" "ssh" {
+  name = "spacelift-demo-sg"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # We'll restrict this later.
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "demo" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.ssh.id]
+
+  tags = {
+    Name        = "web-server"
+    Role        = "web"
+    Environment = "dev"
+    ManagedBy   = "Terraform"
+  }
+}
